b97db0ced2e35c86545bdf744d4f9230348781c9
evrad94 <franckdjeutcheu@gmail.com>
2024-09-05 10:44:00 +0200

    pandas doc

---
+++ pandas/intropandas.py
@@ -0,0 +1,11 @@
+#Pandas
+# Pandas introduce due nuovi tipi di dato
+# Series e DataFrame
+# Series -> prendo in ingresso una lista di valori 
+# DataFrame -> prendono in ingresso un dizionaro o un ndArray
+# DataFrame è un tipo di dato ottimizzato per lavorare con 
+# tabelle bidimensionali come quelle che utiliziamo 
+# su un CSV o in Excel o tramite un JSON o SQL
+# pip install pandas
+
+# vedere file intro_pandas.ipynb
